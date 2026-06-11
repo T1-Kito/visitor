287e9fbcabf694f1cb1fb243c19d33f7d1a14764
danghoanthang <hoanthang0307@gmail.com>
2026-06-11 13:49:56 +0700

    Fix visitor lookup query and lock admin shell

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,6 +14,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="{{ \App\Support\AssetVersion::url('css/admin-ui.css') }}" rel="stylesheet">
+    <link href="{{ \App\Support\AssetVersion::url('css/admin-fixed-shell.css') }}" rel="stylesheet">
     @stack('styles')
 </head>
 <body>
