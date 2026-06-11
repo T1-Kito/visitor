a849862628b7ea81046b1b8c8a521f45ecc4e079
danghoanthang <hoanthang0307@gmail.com>
2026-06-11 14:05:29 +0700

    Track and display visit creator

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -16,6 +16,7 @@ class Visit extends Model
         'code',
         'visitor_id',
         'host_employee_id',
+        'created_by_user_id',
         'scheduled_at',
         'expected_checkout_at',
         'actual_checkin_at',
@@ -48,6 +49,11 @@ class Visit extends Model
     public function hostEmployee(): BelongsTo
     {
         return $this->belongsTo(Employee::class, 'host_employee_id');
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
     }
 
     public function approval(): HasOne
